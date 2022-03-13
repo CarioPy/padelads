b05a44bd08a247fa946069b1d3bca765c6cd9152
Thomas <allezthomas@gmail.com>
2022-03-13 15:19:41 +0000

    Changes13031519

---
+++ _document.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export default function Document() {
+  return (
+    <Head>
+      <title>PadelLads</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+        rel="stylesheet"
+      ></link>
+    </Head>
+  );
+}
